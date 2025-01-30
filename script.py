c41c75ae6914bcd03a3300d1d6d27fccd3c5d996
Mohamed Elshoraky <officialshoraky@gmail.com>
2025-01-30 06:54:47 +0200

    prepare the page path

--- script.py
+++ script.py
@@ -2,3 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 import csv
+
+date = input("Enter the date at the format of MM/DD/YYYY : ")
+pageURL = f"https://www.yallakora.com/match-center/%d9%85%d8%b1%d9%83%d8%b2-%d8%a7%d9%84%d9%85%d8%a8%d8%a7%d8%b1%d9%8a%d8%a7%d8%aa?date={date}#matchesclipNext"
+
